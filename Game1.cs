a3017356fbcb574e9da873f5f17bd3366bdfe79a
Maddox Macnamara <tpmac001maddog@gmail.com>
2023-04-12 11:03:21 -0400

    working through animation

--- Game1.cs
+++ Game1.cs
@@ -1,12 +1,21 @@
 ﻿using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
+using System;
 
 namespace Lesson_3___Animation
 {
     public class Game1 : Game
     {
+        Random generator = new Random();
 
+        Texture2D tribbleGreyTexture, tribbleBrownTexture, tribbleCreamTexture, tribbleOrangeTexture;
+
+        Rectangle tribbleGreyRect, tribbleBrownRect, tribbleCreamRect, tribbleOrangeRect;
+
+        Vector2 tribbleGreySpeed, tribbleBrownSpeed, tribbleCreamSpeed, tribbleOrangeSpeed;
+
+        Color backColor;
 
         private GraphicsDeviceManager _graphics;
         private SpriteBatch _spriteBatch;
@@ -22,6 +31,20 @@ namespace Lesson_3___Animation
         {
             // TODO: Add your initialization logic here
 
+            _graphics.PreferredBackBufferWidth = 800;
+            _graphics.PreferredBackBufferHeight = 600;
+            _graphics.ApplyChanges();
+            this.Window.Title = "Animation";
+
+            tribbleGreyRect = new Rectangle(300, 10, 100, 100);
+            tribbleBrownRect = new Rectangle(400, 10, 100, 100);
+            tribbleCreamRect = new Rectangle(500, 10, 100, 100);
+            tribbleOrangeRect = new Rectangle(600, 10, 100, 100);
+
+            backColor= Color.White;
+
+            tribbleGreySpeed = new Vector2(2, 2);
+
             base.Initialize();
         }
 
@@ -30,6 +53,11 @@ namespace Lesson_3___Animation
             _spriteBatch = new SpriteBatch(GraphicsDevice);
 
             // TODO: use this.Content to load your game content here
+
+            tribbleGreyTexture = Content.Load<Texture2D>("tribbleGrey");
+            tribbleBrownTexture = Content.Load<Texture2D>("tribbleBrown");
+            tribbleCreamTexture = Content.Load<Texture2D>("tribbleCream");
+            tribbleOrangeTexture = Content.Load<Texture2D>("tribbleOrange");
         }
 
         protected override void Update(GameTime gameTime)
@@ -38,15 +66,33 @@ namespace Lesson_3___Animation
                 Exit();
 
             // TODO: Add your update logic here
+            tribbleGreyRect.X += (int)tribbleGreySpeed.X;
+            tribbleGreyRect.Y += (int)tribbleGreySpeed.Y;
+
+            if (tribbleGreyRect.Right > _graphics.PreferredBackBufferWidth || tribbleGreyRect.Left < 0)
+            {
+                tribbleGreySpeed.X *= -1;
+                backColor = Color.Gray;
+            }
+            if (tribbleGreyRect.Bottom > _graphics.PreferredBackBufferHeight || tribbleGreyRect.Top < 0)
+            {
+                tribbleGreySpeed.Y *= -1;
+                backColor = Color.Gray;
+            }
 
             base.Update(gameTime);
         }
 
         protected override void Draw(GameTime gameTime)
         {
-            GraphicsDevice.Clear(Color.CornflowerBlue);
+            GraphicsDevice.Clear(backColor);
 
             // TODO: Add your drawing code here
+
+            _spriteBatch.Begin();
+            _spriteBatch.Draw(tribbleGreyTexture, tribbleGreyRect, Color.White);
+            _spriteBatch.Draw(tribbleBrownTexture, tribbleBrownRect, Color.White);
+            _spriteBatch.End();
 
             base.Draw(gameTime);
         }
